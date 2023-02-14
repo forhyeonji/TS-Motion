04b5e3e2e587afaaf005c4106de8e5cecc841501
forhyeonji <ghkdguswl3@gmail.com>
2023-02-15 00:40:41 +0900

    🍏 fix - item class 만들기

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import { ImageComponent } from './components/page/item/image.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
+import { PageComponent } from './components/page/page.js';
+
+class App {
+  private readonly page: PageComponent;
+  constructor(appRoot: HTMLElement) {
+    this.page = new PageComponent();
+    this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      'Image Title',
+      'http://picsum.photos/600/300'
+    );
+    image.attachTo(appRoot, 'beforeend');
+
+    const video = new VideoComponent(
+      'Video Title',
+      'https://www.youtube.com/watch?v=D6NXJqV8-uc'
+    );
+    video.attachTo(appRoot, 'beforeend');
+
+    const note = new NoteComponent('Note Title', 'Note Body');
+    note.attachTo(appRoot, 'beforeend');
+
+    const todo = new TodoComponent('Todo Title', 'Todo Body');
+    todo.attachTo(appRoot, 'beforeend');
+  }
+}
+
+new App(document.querySelector('.document')! as HTMLElement);
